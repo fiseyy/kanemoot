1a51218263dabfe494dbad62d56220eeba4c7cc3
Daniil Diakov <fiseyy_dev@mail.ru>
2025-09-03 16:52:21 +0300

    Исправлена ошибка сервера: 'ChannelRepository' object has no attribute 'get_by_server'

--- server/main_service/repositories/channels.py
+++ server/main_service/repositories/channels.py
@@ -6,3 +6,8 @@ class ChannelRepository:
         channel = db.query(Channel).filter(Channel.id == channel_id).first()
         db.close()
         return channel
+    def get_by_server(self, channel_id: int):
+        db = SessionLocal()
+        channel = db.query(Channel).filter(Channel.id == channel_id).first()
+        db.close()
+        return channel
