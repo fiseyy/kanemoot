aa7daed62a89cec657de30061added3fb9282c55
Daniil Diakov <fiseyy_dev@mail.ru>
2025-09-03 16:35:31 +0300

    Исправлена ошибка сервера: 'ChannelRepository' object has no attribute 'get'

--- server/main_service/repositories/channels.py
+++ server/main_service/repositories/channels.py
@@ -1,11 +1,8 @@
 from models import SessionLocal, Channel
 
 class ChannelRepository:
-    def __init__(self):
-        self.db = SessionLocal
-
-    def get_by_server(self, server_id: int):
-        db = self.db()
-        channels = db.query(Channel).filter(Channel.server_id == server_id).all()
+    def get(self, channel_id: int):
+        db = SessionLocal()
+        channel = db.query(Channel).filter(Channel.id == channel_id).first()
         db.close()
-        return channels
+        return channel
